5bb7298910cf0c8f8e02d443992694dea3e64594
rafael <rloggiodice@eservicios.indra.es>
2024-06-05 18:13:28 +0200

    Initial Commit

---
+++ Calmify/CalmifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalmifyApp.swift
+//  Calmify
+//
+//  Created by Rafael Loggiodice on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CalmifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
