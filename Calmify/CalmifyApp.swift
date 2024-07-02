5da165a078f017ccce72605ca137ad5daa2e8653
rafael <rloggiodice@eservicios.indra.es>
2024-07-02 09:05:56 +0200

    [feature] add initial configuration of firebase to project

--- Calmify/CalmifyApp.swift
+++ Calmify/CalmifyApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct CalmifyApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             CoordinatorView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }
